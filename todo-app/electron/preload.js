439828100d7969863839f8537cfe462eb327d1c5
CodeNode <codenode@foxmail.com>
2026-04-18 22:51:15 +0800

    Revert "Merge pull request #1 from Eternal4869/electronvue重构与ip工具-19ed2"
    This reverts commit 46d3fe2efd3a01ba6d1b75d7926911255e21e60a, reversing
    changes made to 25960df9b30c513ba796d0f9d7aaffee20f0047f.

--- todo-app/electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getAllIPs: () => ipcRenderer.invoke('get-all-ips'),
-  copyToClipboard: (text) => ipcRenderer.invoke('copy-to-clipboard', text)
-});
